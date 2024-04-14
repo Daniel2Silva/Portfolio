095ac1109603c880bbe83cb50e31a95bd1f14191
Daniel Silva <dn2fe@icloud.com>
2024-04-14 20:35:55 -0300

    adicionei imagem na apresentacao

--- src/Components/main/Apresentacao.jsx
+++ src/Components/main/Apresentacao.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './Apresentacao.module.css';
+import Perfil from '../../assets/perfil.png';
 
 const Apresentacao = () => {
   return (
     <section className={styles.container}>
+      <img src={Perfil} alt="" />
       <h1 className={`${styles.apresentacao} tittleApresentacao`}>
         Olá! Sou Daniel, Desenvolvedor Front End{' '}
       </h1>
