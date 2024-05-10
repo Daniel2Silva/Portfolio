ad933d23a516ecbeb8e34177b699e03d234b3d77
Daniel <danicater@hotmail.com>
2024-05-10 19:13:02 -0300

    melhorias no posrtfolio e novo projeto

--- src/Components/main/Projetos.jsx
+++ src/Components/main/Projetos.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './ProjetosCard.module.css';
 import Button from '../Button/Button';
-import ImgProjeto1 from '../../assets/projeto1.jpg';
-
+import ImgProjeto1 from '../../assets/projeto1.png';
+import ImgProjeto2 from '../../assets/projeto2.png';
 const Projetos = () => {
   return (
-    <section id="projetos">
+    <section id="projetos" className={styles.pjts}>
       <div className={styles.projetos}>
         <h2>Meus Projetos</h2>
         <p>
@@ -17,22 +17,48 @@ const Projetos = () => {
       <section className={styles.projetoCard}>
         <div className={styles.leftCard}>
           <div className={styles.infoCard}>
-            <h3>Imobiliária</h3>
+            <h3>Ecommerce</h3>
             <span>2024 - Front End</span>
             <p>
-              No projeto VILLA, uma corretora de imóveis, desenvolvi o site
-              sozinho usando HTML, CSS e JavaScript para a interface, e
-              implementei um banco de dados com PHP e MySQL para gerenciar
-              informações como imóveis e cadastros de usuários.
+              O site oferece aos usuários uma experiência completa de compras
+              online, permitindo encontrar uma ampla variedade de produtos,
+              criar contas de usuário personalizadas e desfrutar de uma
+              interface intuitiva e amigável para navegação e compra. <br />
+              <br />
+              Tecnologias : React.JS,HTML5, CSS3, Javascript, controle de versão
+              : git
             </p>
           </div>
           <Button
-            link={'https://daniel2silva.github.io/villa1/'}
+            link={'https://daniel2silva.github.io/redstore2/'}
             nome="Acessar Projeto"
           ></Button>
         </div>
         <div className={styles.imgCard}>
           <img src={ImgProjeto1} alt="" />
+        </div>
+      </section>
+      <section className={styles.projetoCard}>
+        <div className={styles.leftCard}>
+          <div className={styles.infoCard}>
+            <h3>Streaming</h3>
+            <span>2024 - Front End</span>
+            <p>
+              O site permite que o usuário explore uma ampla variedade de filmes
+              e séries, além de adicionar e remover itens facilmente de sua
+              lista de favoritos. <br />
+              <br />
+              Tecnologias : React.JS,HTML5, Sass, Javascript, controle de
+              versão: git
+            </p>
+          </div>
+          <Button
+            link={'https://daniel2silva.github.io/redstore2/'}
+            nome="Acessar Projeto"
+          ></Button>
+        </div>
+        <div className={styles.imgCard}>
+          <img src={ImgProjeto2} alt="" />
         </div>
       </section>
     </section>
